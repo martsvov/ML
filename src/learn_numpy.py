41ce4fa3fe0234dc37817dbe07cb9a97bea6a521
martsvov <martsvov@ukr.net>
2020-02-03 21:38:57 +0200

    1

---
+++ src/learn_numpy.py
@@ -0,0 +1,14 @@
+import numpy as np
+import array
+
+a = np.arange(1, 9)
+
+np.less(3)
+
+print(np.arange(3))
+print(np.ones((2, 3)))
+# print(a)
+# print(np.std(a))
+# print(np.var(a))
+# print(np.median(a))
+# print(np.percentile(a, q=50))
