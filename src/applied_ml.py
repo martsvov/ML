8713338c0b7d7c4a310bc0ba184e119c1a1bf23e
volodymyr.martsyniuk <Yamaha2727>
2020-02-11 17:03:20 +0200

    1

---
+++ src/applied_ml.py
@@ -0,0 +1,43 @@
+import os
+import tarfile
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from six.moves import urllib
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+HOUSING_PATH = os.path.join("..\datasets", "housing")
+
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+
+def fetch_housing_data(housing_url=HOUSING_URL,
+                       housing_path=HOUSING_PATH):
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+
+
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+
+fetch_housing_data(HOUSING_URL, HOUSING_PATH)
+df = load_housing_data(HOUSING_PATH)
+# df.hist(bins=50, figsize=(20, 15))
+# plt.show()
+
+train_set, test_set = split_train_test(df, 0.2)
