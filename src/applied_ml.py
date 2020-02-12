e688c48fa0b172bd80b6ed61a18d7120ae4c9acb
volodymyr.martsyniuk <Yamaha2727>
2020-02-12 10:59:23 +0200

    1

--- src/applied_ml.py
+++ src/applied_ml.py
@@ -2,6 +2,9 @@ import os
 import tarfile
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
+import hashlib
 import matplotlib.pyplot as plt
 from six.moves import urllib
 
@@ -35,9 +38,48 @@ def split_train_test(data, test_ratio):
     return data.iloc[train_indices], data.iloc[test_indices]
 
 
+def test_set_check(identifier, test_ratio, hash):
+    return hash(np.int64(identifier)).digest()[-1] < 256 * test_ratio
+
+
+def split_train_test_by_id(data, test_ratio, id_column,
+                           hash=hashlib.md5):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio, hash))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+
 fetch_housing_data(HOUSING_URL, HOUSING_PATH)
 df = load_housing_data(HOUSING_PATH)
 # df.hist(bins=50, figsize=(20, 15))
 # plt.show()
 
-train_set, test_set = split_train_test(df, 0.2)
+# df_with_id = df.reset_index()
+# train_set, test_set = split_train_test(df, 0.2)
+# train_set, test_set = split_train_test_by_id(df_with_id, 0.2, 'index')
+
+df['income_cat'] = np.ceil(df['median_income'] / 1.5)
+df['income_cat'].where(df['income_cat'] < 5, 5.0, inplace=True)
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+
+for train_index, test_index in split.split(df, df['income_cat']):
+    strat_train_set = df.loc[train_index]
+    strat_test_set = df.loc[test_index]
+
+# print(df['income_cat'].value_counts() / len(df))
+# print(strat_train_set['income_cat'].value_counts() / len(strat_train_set))
+# print(strat_test_set['income_cat'].value_counts() / len(strat_test_set))
+
+# train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+housing = strat_train_set.copy()
+# housing.plot(kind='scatter', x='longitude', y='latitude', alpha=0.1)
+housing.plot(kind='scatter', x='longitude', y='latitude', alpha=0.4,
+             s=housing['population'] / 100, label='population',
+             figsize=(10, 7), c='median_house_value',
+             cmap=plt.get_cmap('jet'), colorbar=True,
+             )
+
+plt.legend()
+plt.show()
