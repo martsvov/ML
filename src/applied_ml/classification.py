2b098f90635e712ac10304973443f8da226b0ca1
volodymyr.martsyniuk <Yamaha2727>
2020-02-18 18:03:57 +0200

    1

--- src/applied_ml/classification.py
+++ src/applied_ml/classification.py
@@ -4,8 +4,10 @@ import matplotlib
 import matplotlib.pyplot as plt
 from sklearn.datasets import fetch_openml
 from sklearn.linear_model import SGDClassifier
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import StratifiedKFold, cross_val_score, cross_val_predict
 from sklearn.base import clone
+from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score,\
+    precision_recall_curve, roc_curve, roc_auc_score
 
 
 mnist = fetch_openml('mnist_784')
@@ -28,18 +30,50 @@ y_test_9 = (y_test == 9)
 sgd_clf = SGDClassifier(random_state=42)
 sgd_clf.fit(X_train, y_train_9)
 
-res = sgd_clf.predict([some_digit])
+# skfolds = StratifiedKFold(n_splits=3, random_state=42)
+# for train_index, test_index in skfolds.split(X_train, y_train_9):
+#     clone_clf = clone(sgd_clf)
+#     X_train_folds = X_train[train_index]
+#     y_train_folds = y_train_9[train_index]
+#     X_test_fold = X_train[test_index]
+#     y_test_fold = y_train_9[test_index]
+#     clone_clf.fit(X_train_folds, y_train_folds)
+#     y_pred = clone_clf.predict(X_test_fold)
+#     n_correct = sum(y_pred == y_test_fold)
+#     print(n_correct/len(y_pred))
+cvs = cross_val_score(sgd_clf, X_train, y_train_9, cv=3, scoring="accuracy")
 
-skfolds = StratifiedKFold(n_splits=3, random_state=42)
-for train_index, test_index in skfolds.split(X_train, y_train_9):
-    clone_clf = clone(sgd_clf)
-    X_train_folds = X_train[train_index]
-    y_train_folds = y_train_9[train_index]
-    X_test_fold = X_train[test_index]
-    y_test_fold = y_train_9[test_index]
-    clone_clf.fit(X_train_folds, y_train_folds)
-    y_pred = clone_clf.predict(X_test_fold)
-    n_correct = sum(y_pred == y_test_fold)
-    print(n_correct/len(y_pred))
+y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_9, cv=3)
+# res = sgd_clf.predict(X_train)
+# cm = confusion_matrix(y_train_9, y_train_pred)
+# print(precision_score(y_train_9, y_train_pred))
+# print(recall_score(y_train_9, y_train_pred))
+# print(f1_score(y_train_9, y_train_pred))
 
+y_scores = cross_val_predict(sgd_clf, X_train, y_train_9, cv=3, method="decision_function")
+
+# def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
+#     plt.plot(thresholds, precisions[:-1], "b--", label="Точность")
+#     plt.plot(thresholds, recalls[:-1], "g-", label="Полнота")
+#     plt.xlabel("Пopoг")
+#     plt.legend(loc="center left")
+#     plt.ylim([0, 1])
+#
+# precisions, recalls, thresholds = precision_recall_curve(y_train_9, y_scores)
+# plot_precision_recall_vs_threshold(precisions, recalls, thresholds)
+
+
+# def plot_roc_curve(fpr, tpr, label=None):
+#     plt.plot(fpr, tpr, linewidth=2, label=label)
+#     plt.plot([0, 1], [0, 1], 'k--')
+#     plt.axis([0, 1, 0, 1])
+#     plt.xlabel('False Positive Rate')
+#     plt.ylabel('True Positive Rate')
+#
+# fpr, tpr, thresholds = roc_curve(y_train_9, y_scores)
+# plot_roc_curve(fpr, tpr)
+
+print(roc_auc_score(y_train_9, y_scores))
+
+# plt.show()
 print()
