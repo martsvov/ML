3a081d277bce4a7ef9ef5f704c46c276746ebb11
martsvov <martsvov@ukr.net>
2020-02-25 22:57:23 +0200

    1

--- src/deep_learning.py
+++ src/deep_learning.py
@@ -1,36 +1,127 @@
 import numpy as np
 
 
-def w_sum(a,b):
-    assert(len(a) == len(b))
-    output = 0
-    for i in range(len(a)):
-        output += (a[i] * b[i])
-    return output
-
-
-def vect_mat_mul(vect, matrix):
-    assert(len(vect) == len(matrix))
-    output = [0,0,0]
-    for i in range(len(vect)):
-        output[i] = w_sum(vect, matrix[i])
-    return output
-
-
-def neural_network(input, weights):
-    pred = vect_mat_mul(input, weights)
-    return pred
-
-
-weights = [[0.1, 0.1, -0.3],
-           [0.1, 0.2, 0.0],
-           [0.0, 1.3, 0.1]]
-
-toes = [8.5, 9.5, 9.9, 9.0]
-wlrec = [0.65, 0.8, 0.8, 0.9]
-nfans = [1.2, 1.3, 0.5, 1.0]
-
-input = [toes[0], wlrec[0], nfans[0]]
+# def w_sum(a,b):
+#     assert(len(a) == len(b))
+#     output = 0
+#     for i in range(len(a)):
+#         output += (a[i] * b[i])
+#     return output
+#
+#
+# def vect_mat_mul(vect, matrix):
+#     assert(len(vect) == len(matrix))
+#     output = [0,0,0]
+#     for i in range(len(vect)):
+#         output[i] = w_sum(vect, matrix[i])
+#     return output
+#
+#
+# def neural_network(input, weights):
+#     pred = vect_mat_mul(input, weights)
+#     return pred
+#
+#
+# weights = [[0.1, 0.1, -0.3],
+#            [0.1, 0.2, 0.0],
+#            [0.0, 1.3, 0.1]]
+#
+# toes = [8.5, 9.5, 9.9, 9.0]
+# wlrec = [0.65, 0.8, 0.8, 0.9]
+# nfans = [1.2, 1.3, 0.5, 1.0]
+#
+# input = [toes[0], wlrec[0], nfans[0]]
 # pred = neural_network(input, weights)
 
+# weight = 0.5
+# goal_pred = 0.8
+# input = 0.5
+#
+# for iteration in range(20):
+#     pred = input * weight
+#     error = (pred - goal_pred) ** 2
+#     direction_and_amount = (pred - goal_pred) * input
+#     weight = weight - direction_and_amount
+#     print("Error:" + str(error) + " Prediction:" + str(pred))
+
 # print(pred)
+
+# np.random.seed(1)
+#
+# def relu(x):
+#     return (x > 0) * x
+#
+# def relu2deriv(output):
+#     return output > 0
+#
+#
+# streetlights = np.array([[1, 0, 1],
+#                          [0, 1, 1],
+#                          [0, 0, 1],
+#                          [1, 1, 1]])
+# walk_vs_stop = np.array([[1, 1, 0, 0]]).T
+# alpha = 0.2
+# hidden_size = 4
+# weights_0_1 = 2*np.random.random((3, hidden_size)) - 1
+# weights_1_2 = 2*np.random.random((hidden_size, 1)) - 1
+#
+# for iteration in range(60):
+#     layer_2_error = 0
+#     for i in range(len(streetlights)):
+#         layer_0 = streetlights[i:i+1]
+#         layer_1 = relu(np.dot(layer_0, weights_0_1))
+#         layer_2 = np.dot(layer_1, weights_1_2)
+#         layer_2_error += np.sum((layer_2 - walk_vs_stop[i:i+1]) ** 2)
+#         layer_2_delta = (layer_2 - walk_vs_stop[i:i+1])
+#         layer_1_delta = layer_2_delta.dot(weights_1_2.T)*relu2deriv(layer_1)
+#         weights_1_2 -= alpha * layer_1.T.dot(layer_2_delta)
+#         weights_0_1 -= alpha * layer_0.T.dot(layer_1_delta)
+#     if iteration % 10 == 9:
+#         print("Error:" + str(layer_2_error))
+
+import sys
+from keras.datasets import mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+images, labels = (x_train[0:1000].reshape(1000, 28*28) / 255, y_train[0:1000])
+
+one_hot_labels = np.zeros((len(labels), 10))
+for i, l in enumerate(labels):
+    one_hot_labels[i][l] = 1
+labels = one_hot_labels
+test_images = x_test.reshape(len(x_test), 28*28) / 255
+test_labels = np.zeros((len(y_test), 10))
+for i, l in enumerate(y_test):
+    test_labels[i][l] = 1
+np.random.seed(1)
+np.seterr(all='raise', over='ignore')
+
+
+def relu(x):
+    # x = np.nan_to_num(x)
+    return (x >= 0) * x
+
+
+relu2deriv = lambda x: x > 0
+alpha, iterations, hidden_size, pixels_per_image, num_labels = (0.005, 350, 40, 784, 10)
+weights_0_1 = 0.2*np.random.random((pixels_per_image, hidden_size)) - 0.1
+weights_1_2 = 0.2*np.random.random((hidden_size, num_labels)) - 0.1
+
+for j in range(iterations):
+    error, correct_cnt = (0.0, 0)
+    for i in range(len(images)):
+        layer_0 = images[i:i+1]
+        layer_1 = relu(np.dot(layer_0, weights_0_1))
+        # dropout_mask = np.random.randint(2, size=layer_1.shape)
+        # layer_1 *= dropout_mask * 2
+        layer_2 = np.dot(layer_1, weights_1_2)
+        error += np.sum((labels[i:i+1] - layer_2) ** 2)
+        correct_cnt += int(np.argmax(layer_2) == np.argmax(labels[i:i+1]))
+        layer_2_delta = (labels[i:i+1] - layer_2)
+        layer_1_delta = layer_2_delta.dot(weights_1_2.T) * relu2deriv(layer_1)
+        # layer_1_delta *= dropout_mask
+        weights_1_2 += alpha * layer_1.T.dot(layer_2_delta)
+        weights_0_1 += alpha * layer_0.T.dot(layer_1_delta)
+sys.stdout.write(" I:" + str(j) +
+                 " Error:" + str(error / float(len(images)))[0:5] +
+                 " Correct:" + str(correct_cnt / float(len(images))))
--- src/python_library.py
+++ src/python_library.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pandas as pd
+import numexpr
+# from pandas_datareader import data
 import matplotlib.pyplot as plt
-import seaborn as sns
+import seaborn as sns; sns.set()
+from datetime import datetime
 import re
 
 # index = [('California', 2000), ('California', 2010),
@@ -91,10 +94,8 @@ import re
 # age = pd.cut(titanic['age'], [0, 18, 80])
 # pt = titanic.pivot_table('survived', ['sex', age], 'class')
 
-sns.set()
-
-births = pd.read_csv('../data/births.csv')
-births['decade'] = 10 * (births['year'] // 10)
+# births = pd.read_csv('../data/births.csv')
+# births['decade'] = 10 * (births['year'] // 10)
 # pt = births.pivot_table('births', index='decade', columns='gender', aggfunc='sum').plot()
 #
 # plt.ylabel('total births per year')
@@ -132,7 +133,227 @@ births['decade'] = 10 * (births['year'] // 10)
 # spice_list = ['salt', 'pepper', 'oregano', 'sage', 'parsley', 'rosemary', 'tarragon', 'thyme', 'paprika', 'cumin']
 # spice_df = pd.DataFrame(dict((spice, recipes.ingredients.str.contains(spice, re.IGNORECASE)) for spice in spice_list))
 
-index = pd.DatetimeIndex(['2014-07-04', '2014-08-04', '2015-07-04', '2015-08-04'])
-data = pd.Series([0, 1, 2, 3], index=index)
+# index = pd.DatetimeIndex(['2014-07-04', '2014-08-04', '2015-07-04', '2015-08-04'])
+# data = pd.Series([0, 1, 2, 3], index=index)
 
-print(data)
+# dates = pd.to_datetime([datetime(2015, 7, 3), '4th of July, 2015', '2015-Jul-6', '07-07-2015', '20150708'])
+# dates = pd.date_range('2015-07-01', periods=8, freq='MS')
+
+# goog = data.DataReader('GOOG', start='2004', end='2016', data_source='stooq')
+# goog = goog['Close']
+# goog = goog.sort_index()
+
+# goog.plot(alpha=0.5, style='-')
+# goog.resample('BA').mean().plot(style=':')
+# goog.asfreq('BA').plot(style='--')
+# plt.legend(['input', 'resample', 'asfreq'], loc='upper left')
+
+# fig, ax = plt.subplots(2, sharex=True)
+# data = goog.iloc[-10:]
+# data.asfreq('D').plot(ax=ax[0], marker='o')
+# data.asfreq('D', method='bfill').plot(ax=ax[1], style='-o')
+# data.asfreq('D', method='ffill').plot(ax=ax[1], style='--o')
+# ax[1].legend(["back-fill", "forward-fill"])
+
+# _, ax = plt.subplots(3, sharey=True)
+# goog = goog.asfreq('D', method='pad')
+# goog.plot(ax=ax[0])
+# goog.shift(900).plot(ax=ax[1])
+# goog.tshift(900).plot(ax=ax[2])
+#
+# local_max = pd.to_datetime('2007-11-05')
+# offset = pd.Timedelta(900, 'D')
+# ax[0].legend(['input'], loc=2)
+# ax[0].get_xticklabels()[4].set(weight='heavy', color='red')
+# ax[0].axvline(local_max, alpha=0.3, color='red')
+# ax[1].legend(['shift(900)'], loc=2)
+# ax[1].get_xticklabels()[4].set(weight='heavy', color='red')
+# ax[1].axvline(local_max + offset, alpha=0.3, color='red')
+# ax[2].legend(['tshift(900)'], loc=2)
+# ax[2].get_xticklabels()[1].set(weight='heavy', color='red')
+# ax[2].axvline(local_max + offset, alpha=0.3, color='red');
+
+# ROI = 100 * (goog.tshift(-365) / goog - 1)
+# ROI.plot()
+# plt.ylabel('% Return on Investment')
+
+# rolling = goog.rolling(365, center=True)
+# data = pd.DataFrame({'input': goog, 'one-year rolling_mean': rolling.mean(), 'one-year rolling_std': rolling.std()})
+# ax = data.plot(style=['-', '--', ':'])
+# ax.lines[0].set_alpha(0.3)
+
+# data = pd.read_csv('../data/FremontBridge.csv', index_col='Date', parse_dates=True)
+# data.columns = ['Total', 'East', 'West']
+# data.plot()
+# plt.ylabel('Hourly Bicycle Count')
+
+# weekly = data.resample('W').sum()
+# weekly.plot(style=[':', '--', '-'])
+# plt.ylabel('Weekly bicycle count')
+
+# daily = data.resample('D').sum()
+# daily.rolling(50, center=True, win_type='gaussian').sum(std=10).plot(style=[':', '--', '-']);
+# plt.ylabel('mean hourly count')
+
+# by_time = data.groupby(data.index.time).mean()
+# hourly_ticks = 4 * 60 * 60 * np.arange(6)
+# by_time.plot(xticks=hourly_ticks, style=[':', '--', '-'])
+
+# weekend = np.where(data.index.weekday < 5, 'Weekday', 'Weekend')
+# by_time = data.groupby([weekend, data.index.time]).mean()
+#
+# fig, ax = plt.subplots(1, 2, figsize=(14, 5))
+# by_time.loc['Weekday'].plot(ax=ax[0], title='Weekdays', xticks=hourly_ticks, style=[':', '--', '-'])
+# by_time.loc['Weekend'].plot(ax=ax[1], title='Weekends', xticks=hourly_ticks, style=[':', '--', '-'])
+
+# nrows, ncols = 100000, 100
+# rng = np.random.RandomState(42)
+# df1, df2 = (pd.DataFrame(rng.rand(nrows, ncols)) for i in range(4))
+
+plt.style.use('seaborn-whitegrid')
+
+# fig = plt.figure()
+# ax = plt.axes()
+#
+# x = np.linspace(0, 10, 50)
+
+# ax.plot(x, np.sin(x))
+# plt.plot(x, x + 0, '-g')
+# plt.plot(x, x + 1, '--c')
+# plt.plot(x, x + 2, '-.k')
+# plt.plot(x, x + 3, ':r')
+
+# plt.plot(x, np.sin(x))
+# plt.axis([-1, 11, -1.5, 1.5])
+# plt.title("A Sine Curve")
+# plt.xlabel("x")
+# plt.ylabel("sin(x)")
+
+# ax.plot(x, np.sin(x), '-g', label='sin(x)')
+# ax.plot(x, np.cos(x), ':b', label='cos(x)')
+# ax.axis('equal')
+# ax.legend()
+
+# ax.plot(x, np.sin(x))
+# ax.set(xlim=(0, 10), ylim=(-2, 2), xlabel='x', ylabel='sin(x)', title='A Simple Plot')
+
+# rng = np.random.RandomState(0)
+# for marker in ['o', '.', ',', 'x', '+', 'v', '^', '<', '>', 's', 'd']:
+#     plt.plot(rng.rand(5), rng.rand(5), marker, label="marker='{0}'".format(marker))
+# plt.legend(numpoints=1)
+# plt.xlim(0, 1.8)
+
+# rng = np.random.RandomState(0)
+# x = rng.randn(100)
+# y = rng.randn(100)
+# colors = rng.rand(100)
+# sizes = 1000 * rng.rand(100)
+# plt.scatter(x, y, c=colors, s=sizes, alpha=0.3, cmap='viridis')
+# plt.colorbar()
+
+# from sklearn.datasets import load_iris
+#
+# iris = load_iris()
+# features = iris.data.T
+# plt.scatter(features[0], features[1], alpha=0.2, s=100*features[3], c=iris.target, cmap='viridis')
+# plt.xlabel(iris.feature_names[0])
+# plt.ylabel(iris.feature_names[1])
+
+# x = np.linspace(0, 10, 50)
+# dy = 0.8
+# y = np.sin(x) + dy * np.random.randn(50)
+# plt.errorbar(x, y, yerr=dy, fmt='.k')
+# plt.errorbar(x, y, yerr=dy, fmt='o', color='black', ecolor='lightgray', elinewidth=3, capsize=0)
+
+# from sklearn.gaussian_process import GaussianProcessRegressor
+#
+# model = lambda x: x * np.sin(x)
+# xdata = np.array([1, 3, 5, 6, 8])
+# ydata = model(xdata)
+#
+# gp = GaussianProcessRegressor()
+# gp.fit(xdata[:, np.newaxis], ydata)
+# xfit = np.linspace(0, 10, 1000)
+# yfit, MSE = gp.predict(xfit[:, np.newaxis], return_std=True)
+# dyfit = 2 * np.sqrt(MSE)
+#
+# plt.plot(xdata, ydata, 'or')
+# plt.plot(xfit, yfit, '-', color='gray')
+# plt.fill_between(xfit, yfit - dyfit, yfit + dyfit, color='gray', alpha=0.2)
+# plt.xlim(0, 10)
+
+# def f(x, y):
+#     return np.sin(x) ** 10 + np.cos(10 + y * x) * np.cos(x)
+#
+#
+# x = np.linspace(0, 5, 50)
+# y = np.linspace(0, 5, 40)
+# X, Y = np.meshgrid(x, y)
+# Z = f(X, Y)
+# plt.contour(X, Y, Z, colors='black')
+# plt.contourf(X, Y, Z, 20, cmap='RdGy')
+# plt.colorbar()
+
+# plt.imshow(Z, extent=[0, 5, 0, 5], origin='lower', cmap='RdGy')
+# plt.colorbar()
+# plt.axis(aspect='image')
+
+# contours = plt.contour(X, Y, Z, 3, colors='black')
+# plt.clabel(contours, inline=True, fontsize=8)
+# plt.imshow(Z, extent=[0, 5, 0, 5], origin='lower', cmap='RdGy', alpha=0.5)
+# plt.colorbar()
+
+# data = np.random.randn(1000)
+# plt.hist(data, bins=30, normed=True, alpha=0.5, histtype='stepfilled', color='steelblue', edgecolor='none')
+
+# x1 = np.random.normal(0, 0.8, 1000)
+# x2 = np.random.normal(-2, 1, 1000)
+# x3 = np.random.normal(3, 2, 1000)
+#
+# kwargs = dict(histtype='stepfilled', alpha=0.3, density=True, bins=40)
+# plt.hist(x1, **kwargs)
+# plt.hist(x2, **kwargs)
+# plt.hist(x3, **kwargs)
+
+# mean = [0, 0]
+# cov = [[1, 1], [1, 2]]
+# x, y = np.random.multivariate_normal(mean, cov, 10000).T
+# plt.hist2d(x, y, bins=30, cmap='Blues')
+# cb = plt.colorbar()
+# cb.set_label('counts in bin')
+#
+# plt.hexbin(x, y, gridsize=30, cmap='Blues')
+# cb = plt.colorbar()
+# cb.set_label('count in bin')
+
+x = np.linspace(0, 10, 1000)
+# fig, ax = plt.subplots()
+# ax.plot(x, np.sin(x), '-b', label='Sine')
+# ax.plot(x, np.cos(x), '--r', label='Cosine')
+# ax.axis('equal')
+# ax.legend(frameon=False, loc='lower center', ncol=2)
+
+# y = np.sin(x[:, np.newaxis] + np.pi * np.arange(0, 2, 0.5))
+# lines = plt.plot(x, y)
+# plt.legend(lines[:2], ['first', 'second'])
+
+cities = pd.read_csv('../data/california_cities.csv')
+
+lat, lon = cities['latd'], cities['longd']
+population, area = cities['population_total'], cities['area_total_km2']
+
+plt.scatter(lon, lat, label=None, c=np.log10(population), cmap='viridis', s=area, linewidth=0, alpha=0.5)
+plt.axis(aspect='equal')
+plt.xlabel('longitude')
+plt.ylabel('latitude')
+plt.colorbar(label='log$_{10}$(population)')
+plt.clim(3, 7)
+
+for area in [100, 300, 500]:
+    plt.scatter([], [], c='k', alpha=0.3, s=area, label=str(area) + ' km$^2$')
+
+plt.legend(scatterpoints=1, frameon=False, labelspacing=1, title='City Area')
+plt.title('California Cities: Area and Population')
+
+plt.show()
+print()
