dd681491b5ef39376b8dbccfcb1a9c7ddd405e4a
martsvov <martsvov@ukr.net>
2020-03-01 21:02:49 +0200

    1

--- src/deep_learning.py
+++ src/deep_learning.py
@@ -1,5 +1,6 @@
-import numpy as np
+import sys
 
+import numpy as np
 
 # def w_sum(a,b):
 #     assert(len(a) == len(b))
@@ -32,7 +33,6 @@ import numpy as np
 #
 # input = [toes[0], wlrec[0], nfans[0]]
 # pred = neural_network(input, weights)
-
 # weight = 0.5
 # goal_pred = 0.8
 # input = 0.5
@@ -43,9 +43,7 @@ import numpy as np
 #     direction_and_amount = (pred - goal_pred) * input
 #     weight = weight - direction_and_amount
 #     print("Error:" + str(error) + " Prediction:" + str(pred))
-
 # print(pred)
-
 # np.random.seed(1)
 #
 # def relu(x):
@@ -78,23 +76,21 @@ import numpy as np
 #         weights_0_1 -= alpha * layer_0.T.dot(layer_1_delta)
 #     if iteration % 10 == 9:
 #         print("Error:" + str(layer_2_error))
-
-import sys
-from keras.datasets import mnist
-
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-images, labels = (x_train[0:1000].reshape(1000, 28*28) / 255, y_train[0:1000])
-
-one_hot_labels = np.zeros((len(labels), 10))
-for i, l in enumerate(labels):
-    one_hot_labels[i][l] = 1
-labels = one_hot_labels
-test_images = x_test.reshape(len(x_test), 28*28) / 255
-test_labels = np.zeros((len(y_test), 10))
-for i, l in enumerate(y_test):
-    test_labels[i][l] = 1
-np.random.seed(1)
-
+# import sys
+# from keras.datasets import mnist
+#
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
+# images, labels = (x_train[0:1000].reshape(1000, 28*28) / 255, y_train[0:1000])
+#
+# one_hot_labels = np.zeros((len(labels), 10))
+# for i, l in enumerate(labels):
+#     one_hot_labels[i][l] = 1
+# labels = one_hot_labels
+# test_images = x_test.reshape(len(x_test), 28*28) / 255
+# test_labels = np.zeros((len(y_test), 10))
+# for i, l in enumerate(y_test):
+#     test_labels[i][l] = 1
+# np.random.seed(1)
 # np.seterr(all='raise', over='ignore')
 #
 #
@@ -126,100 +122,236 @@ np.random.seed(1)
 # sys.stdout.write(" I:" + str(j) +
 #                  " Error:" + str(error / float(len(images)))[0:5] +
 #                  " Correct:" + str(correct_cnt / float(len(images))))
+# def tanh(x):
+#     return np.tanh(x)
+#
+#
+# def tanh2deriv(output):
+#     return 1 - (output ** 2)
+#
+#
+# def softmax(x):
+#     temp = np.exp(x)
+#     return temp / np.sum(temp, axis=1, keepdims=True)
+#
+#
+# alpha, iterations = (2, 300)
+# pixels_per_image, num_labels = (784, 10)
+# batch_size = 128
+# input_rows = 28
+# input_cols = 28
+# kernel_rows = 3
+# kernel_cols = 3
+# num_kernels = 16222
+# hidden_size = ((input_rows - kernel_rows) * (input_cols - kernel_cols)) * num_kernels
+# kernels = 0.02*np.random.random((kernel_rows*kernel_cols, num_kernels))-0.01
+# weights_1_2 = 0.2*np.random.random((hidden_size, num_labels)) - 0.1
+#
+#
+# def get_image_section(layer, row_from, row_to, col_from, col_to):
+#     section = layer[:, row_from:row_to, col_from:col_to]
+#     return section.reshape(-1, 1, row_to-row_from, col_to-col_from)
+#
+#
+# for j in range(iterations):
+#     correct_cnt = 0
+#     for i in range(int(len(images) / batch_size)):
+#         batch_start, batch_end = ((i * batch_size), ((i + 1) * batch_size))
+#         layer_0 = images[batch_start:batch_end]
+#         layer_0 = layer_0.reshape(layer_0.shape[0], 28, 28)
+#         # layer_0.shape
+#         sects = list()
+#         for row_start in range(layer_0.shape[1] - kernel_rows):
+#             for col_start in range(layer_0.shape[2] - kernel_cols):
+#                 sect = get_image_section(layer_0,
+#                                          row_start,
+#                                          row_start + kernel_rows,
+#                                          col_start,
+#                                          col_start + kernel_cols)
+#                 sects.append(sect)
+#
+#         expanded_input = np.concatenate(sects, axis=1)
+#         es = expanded_input.shape
+#         flattened_input = expanded_input.reshape(es[0] * es[1], -1)
+#         kernel_output = flattened_input.dot(kernels)
+#         layer_1 = tanh(kernel_output.reshape(es[0], -1))
+#         dropout_mask = np.random.randint(2, size=layer_1.shape)
+#         layer_1 *= dropout_mask * 2
+#         layer_2 = softmax(np.dot(layer_1, weights_1_2))
+#         for k in range(batch_size):
+#             labelset = labels[batch_start + k:batch_start + k + 1]
+#             _inc = int(np.argmax(layer_2[k:k + 1]) == np.argmax(labelset))
+#             correct_cnt += _inc
+#
+#         layer_2_delta = (labels[batch_start:batch_end] - layer_2) / (batch_size * layer_2.shape[0])
+#         layer_1_delta = layer_2_delta.dot(weights_1_2.T) * tanh2deriv(layer_1)
+#         layer_1_delta *= dropout_mask
+#         weights_1_2 += alpha * layer_1.T.dot(layer_2_delta)
+#         lld_reshape = layer_1_delta.reshape(kernel_output.shape)
+#         k_update = flattened_input.T.dot(lld_reshape)
+#         kernels -= alpha * k_update
+#
+#     test_correct_cnt = 0
+#     for i in range(len(test_images)):
+#         layer_0 = test_images[i:i + 1]
+#         layer_0 = layer_0.reshape(layer_0.shape[0], 28, 28)
+#         # layer_0.shape
+#         sects = list()
+#         for row_start in range(layer_0.shape[1] - kernel_rows):
+#             for col_start in range(layer_0.shape[2] - kernel_cols):
+#                 sect = get_image_section(layer_0,
+#                                          row_start,
+#                                          row_start + kernel_rows,
+#                                          col_start,
+#                                          col_start + kernel_cols)
+#                 sects.append(sect)
+#
+#         expanded_input = np.concatenate(sects, axis=1)
+#         es = expanded_input.shape
+#         flattened_input = expanded_input.reshape(es[0] * es[1], -1)
+#         kernel_output = flattened_input.dot(kernels)
+#         layer_1 = tanh(kernel_output.reshape(es[0], -1))
+#         layer_2 = np.dot(layer_1, weights_1_2)
+#         test_correct_cnt += int(np.argmax(layer_2) == np.argmax(test_labels[i:i + 1]))
+#         if j % 1 == 0:
+#             sys.stdout.write("I:" + str(j) +
+#                              "Test-Acc:" + str(test_correct_cnt / float(len(test_images))) +
+#                              "Train-Acc:" + str(correct_cnt/float(len(images))))
+
+f = open('../data/reviews.txt')
+raw_reviews = f.readlines()
+f.close()
+
+f = open('../data/labels.txt')
+raw_labels = f.readlines()
+f.close()
+
+tokens = list(map(lambda x:set(x.split(" ")), raw_reviews))
+vocab = set()
+for sent in tokens:
+    for word in sent:
+        if len(word)>0:
+            vocab.add(word)
+vocab = list(vocab)
+
+word2index = {}
+for i, word in enumerate(vocab):
+    word2index[word] = i
+
+# input_dataset = list()
+# for sent in tokens:
+#     sent_indices = list()
+#     for word in sent:
+#         try:
+#             sent_indices.append(word2index[word])
+#         except:
+#             ""
+#     input_dataset.append(list(set(sent_indices)))
+#
+# target_dataset = list()
+# for label in raw_labels:
+#     if label == 'positive\n':
+#         target_dataset.append(1)
+#     else:
+#         target_dataset.append(0)
+#
+# np.random.seed(1)
+
+from collections import Counter
+import math, random
 
 
-def tanh(x):
-    return np.tanh(x)
+def sigmoid(x):
+    return 1/(1 + np.exp(-x))
 
 
-def tanh2deriv(output):
-    return 1 - (output ** 2)
+# alpha, iterations = (0.01, 2)
+# hidden_size = 100
+# weights_0_1 = 0.2*np.random.random((len(vocab), hidden_size)) - 0.1
+# weights_1_2 = 0.2*np.random.random((hidden_size, 1)) - 0.1
+# correct, total = (0, 0)
+#
+# for iter in range(iterations):
+#     for i in range(len(input_dataset)-1000):
+#         x, y = (input_dataset[i], target_dataset[i])
+#         layer_1 = sigmoid(np.sum(weights_0_1[x], axis=0))
+#         layer_2 = sigmoid(np.dot(layer_1, weights_1_2))
+#         layer_2_delta = layer_2 - y
+#         layer_1_delta = layer_2_delta. dot(weights_1_2.T)
+#         weights_0_1[x] -= layer_1_delta * alpha
+#         weights_1_2 -= np.outer(layer_1, layer_2_delta) * alpha
+#         if np.abs(layer_2_delta) < 0.5:
+#             correct += 1
+#         total += 1
+#         if i % 10 == 9:
+#             progress = str(i / float(len(input_dataset)))
+#             sys.stdout.write('\rlter:' + str(iter)
+#                              + ' Progress:' + progress[2:4]
+#                              + '.' + progress[4:6]
+#                              + '% Training Accuracy:'
+#                              + str(correct/float(total)) + '%')
+#     print()
+#
+# correct, total = (0, 0)
+# for i in range(len(input_dataset)-1000, len(input_dataset)):
+#     x = input_dataset[i]
+#     y = target_dataset[i]
+#     layer_l = sigmoid(np.sum(weights_0_1[x], axis=0))
+#     layer_2 = sigmoid(np.dot(layer_l, weights_1_2))
+#     if np.abs(layer_2 - y) < 0.5:
+#         correct += 1
+#     total += 1
+#
+# print("Test Accuracy:" + str(correct / float(total)))
 
 
-def softmax(x):
-    temp = np.exp(x)
-    return temp / np.sum(temp, axis=1, keepdims=True)
+concatenated = list()
+input_dataset = list()
+
+for sent in tokens:
+    sent_indices = list()
+    for word in sent:
+        try:
+            sent_indices.append(word2index[word])
+            concatenated.append(word2index[word])
+        except:
+            ""
+    input_dataset.append(sent_indices)
+concatenated = np.array(concatenated)
+
+random.shuffle(input_dataset)
+alpha, iterations = (0.05, 2)
+hidden_size, window, negative = (50, 2, 5)
+weights_0_1 = (np.random.rand(len(vocab), hidden_size) - 0.5) * 0.2
+weights_1_2 = np.random.rand(len(vocab), hidden_size) * 0
+layer_2_target = np.zeros(negative + 1)
+layer_2_target[0] = 1
 
 
-alpha, iterations = (2, 300)
-pixels_per_image, num_labels = (784, 10)
-batch_size = 128
-input_rows = 28
-input_cols = 28
-kernel_rows = 3
-kernel_cols = 3
-num_kernels = 16222
-hidden_size = ((input_rows - kernel_rows) * (input_cols - kernel_cols)) * num_kernels
-kernels = 0.02*np.random.random((kernel_rows*kernel_cols, num_kernels))-0.01
-weights_1_2 = 0.2*np.random.random((hidden_size, num_labels)) - 0.1
+def similar(target='beautiful'):
+    target_index = word2index[target]
+    scores = Counter()
+    for word, index in word2index.items():
+        raw_difference = weights_0_1[index] - (weights_0_1[target_index])
+        squared_difference = raw_difference * raw_difference
+        scores[word] = -math.sqrt(sum(squared_difference))
+    return scores.most_common(10)
 
 
-def get_image_section(layer, row_from, row_to, col_from, col_to):
-    section = layer[:, row_from:row_to, col_from:col_to]
-    return section.reshape(-1, 1, row_to-row_from, col_to-col_from)
+for rev_i, review in enumerate(input_dataset * iterations):
+    for target_i in range(len(review)):
+        target_samples = [review[target_i]] + list(concatenated[(np.random.rand(negative) *
+                                                                 len(concatenated)).astype('int').tolist()])
+        left_context = review[max(0, target_i - window):target_i]
+        right_context = review[target_i+1:min(len(review), target_i + window)]
+        layer_1 = np.mean(weights_0_1[left_context + right_context], axis=0)
+        layer_2 = sigmoid(layer_1.dot(weights_1_2[target_samples].T))
+        layer_2_delta = layer_2 - layer_2_target
+        layer_1_delta = layer_2_delta.dot(weights_1_2[target_samples])
+        weights_0_1[left_context + right_context] -= layer_1_delta * alpha
+        weights_1_2[target_samples] -= np.outer(layer_2_delta, layer_1) * alpha
 
+    if rev_i % 250 == 0:
+       sys.stdout.write('\rProgress:' + str(rev_i / float(len(input_dataset) * iterations)))
 
-for j in range(iterations):
-    correct_cnt = 0
-    for i in range(int(len(images) / batch_size)):
-        batch_start, batch_end = ((i * batch_size), ((i + 1) * batch_size))
-        layer_0 = images[batch_start:batch_end]
-        layer_0 = layer_0.reshape(layer_0.shape[0], 28, 28)
-        # layer_0.shape
-        sects = list()
-        for row_start in range(layer_0.shape[1] - kernel_rows):
-            for col_start in range(layer_0.shape[2] - kernel_cols):
-                sect = get_image_section(layer_0,
-                                         row_start,
-                                         row_start + kernel_rows,
-                                         col_start,
-                                         col_start + kernel_cols)
-                sects.append(sect)
-
-        expanded_input = np.concatenate(sects, axis=1)
-        es = expanded_input.shape
-        flattened_input = expanded_input.reshape(es[0] * es[1], -1)
-        kernel_output = flattened_input.dot(kernels)
-        layer_1 = tanh(kernel_output.reshape(es[0], -1))
-        dropout_mask = np.random.randint(2, size=layer_1.shape)
-        layer_1 *= dropout_mask * 2
-        layer_2 = softmax(np.dot(layer_1, weights_1_2))
-        for k in range(batch_size):
-            labelset = labels[batch_start + k:batch_start + k + 1]
-            _inc = int(np.argmax(layer_2[k:k + 1]) == np.argmax(labelset))
-            correct_cnt += _inc
-
-        layer_2_delta = (labels[batch_start:batch_end] - layer_2) / (batch_size * layer_2.shape[0])
-        layer_1_delta = layer_2_delta.dot(weights_1_2.T) * tanh2deriv(layer_1)
-        layer_1_delta *= dropout_mask
-        weights_1_2 += alpha * layer_1.T.dot(layer_2_delta)
-        lld_reshape = layer_1_delta.reshape(kernel_output.shape)
-        k_update = flattened_input.T.dot(lld_reshape)
-        kernels -= alpha * k_update
-
-    test_correct_cnt = 0
-    for i in range(len(test_images)):
-        layer_0 = test_images[i:i + 1]
-        layer_0 = layer_0.reshape(layer_0.shape[0], 28, 28)
-        # layer_0.shape
-        sects = list()
-        for row_start in range(layer_0.shape[1] - kernel_rows):
-            for col_start in range(layer_0.shape[2] - kernel_cols):
-                sect = get_image_section(layer_0,
-                                         row_start,
-                                         row_start + kernel_rows,
-                                         col_start,
-                                         col_start + kernel_cols)
-                sects.append(sect)
-
-        expanded_input = np.concatenate(sects, axis=1)
-        es = expanded_input.shape
-        flattened_input = expanded_input.reshape(es[0] * es[1], -1)
-        kernel_output = flattened_input.dot(kernels)
-        layer_1 = tanh(kernel_output.reshape(es[0], -1))
-        layer_2 = np.dot(layer_1, weights_1_2)
-        test_correct_cnt += int(np.argmax(layer_2) == np.argmax(test_labels[i:i + 1]))
-        if j % 1 == 0:
-            sys.stdout.write("I:" + str(j) +
-                             "Test-Acc:" + str(test_correct_cnt / float(len(test_images))) +
-                             "Train-Acc:" + str(correct_cnt/float(len(images))))
+print(similar('terrible'))
